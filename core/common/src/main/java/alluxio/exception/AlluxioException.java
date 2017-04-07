028994dd6c6b3fb5ff90b47c57dffbcc967dde78
Andrew Audibert <andrew@alluxio.com>
2017-04-06 22:47:56 -0700

    Add thrift status exception and update AlluxioException

--- core/common/src/main/java/alluxio/exception/AlluxioException.java
+++ core/common/src/main/java/alluxio/exception/AlluxioException.java
@@ -11,167 +11,34 @@
 
 package alluxio.exception;
 
-import alluxio.thrift.AlluxioTException;
+import alluxio.exception.status.AlluxioStatusException;
 
 import javax.annotation.concurrent.ThreadSafe;
 
 /**
- * General {@link AlluxioException} used throughout the system. It must be able serialize itself to
- * the RPC framework and convert back without losing any necessary information.
+ * Legacy exception type thrown by the client API. All exceptions thrown from Alluxio servers will
+ * extend this type. In 2.0 this class will be removed and {@link AlluxioStatusException} will
+ * extend RuntimeException directly.
+ *
+ * @deprecated use {@link AlluxioStatusException} instead
  */
 @ThreadSafe
-public class AlluxioException extends Exception {
+@Deprecated
+public class AlluxioException extends RuntimeException {
   private static final long serialVersionUID = 2243833925609642384L;
 
   /**
-   * Constructs a {@link AlluxioException} with an exception type from a {@link AlluxioTException}.
-   *
-   * @param te the type of the exception
+   * @param message the exception message
    */
-  protected AlluxioException(AlluxioTException te) {
-    super(te.getMessage());
-  }
-
-  /**
-   * Constructs an {@link AlluxioException} with the given cause.
-   *
-   * @param cause the cause
-   */
-  protected AlluxioException(Throwable cause) {
-    super(cause);
-  }
-
-  /**
-   * Constructs an {@link AlluxioException} with the given message.
-   *
-   * @param message the message
-   */
-  protected AlluxioException(String message) {
+  public AlluxioException(String message) {
     super(message);
   }
 
   /**
-   * Constructs an {@link AlluxioException} with the given message and cause.
-   *
-   * @param message the message
-   * @param cause the cause
+   * @param message the exception message
+   * @param cause the cause of the exception
    */
-  protected AlluxioException(String message, Throwable cause) {
+  public AlluxioException(String message, Throwable cause) {
     super(message, cause);
-  }
-
-  /**
-   * Constructs a {@link AlluxioTException} from a {@link AlluxioException}.
-   *
-   * @return a {@link AlluxioTException} of the type of this exception
-   */
-  public AlluxioTException toThrift() {
-    return new AlluxioTException(AlluxioExceptionType.getAlluxioExceptionType(getClass()),
-        getMessage(), getClass().getName());
-  }
-
-  /**
-   * Converts an Alluxio exception from Thrift representation to native representation.
-   *
-   * @param e the Alluxio Thrift exception
-   * @return the native Alluxio exception
-   */
-  public static AlluxioException fromThrift(AlluxioTException e) {
-    Class<? extends AlluxioException> throwClass;
-    if (e.isSetClassName()) {
-      // server version 1.1.0 or newer
-      try {
-        throwClass = (Class<? extends AlluxioException>) Class.forName(e.getClassName());
-      } catch (ClassNotFoundException ee) {
-        // this can happen when the client is talking to a newer version of a server that
-        // introduced an exception that the client does not recognize
-        return new AlluxioException(e.getMessage());
-      }
-    } else {
-      // server version 1.0.x
-      throwClass = AlluxioExceptionType.getAlluxioExceptionClass(e.getType());
-      if (throwClass == null) {
-        return new AlluxioException(e.getMessage());
-      }
-    }
-    try {
-      return throwClass.getConstructor(String.class).newInstance(e.getMessage());
-    } catch (ReflectiveOperationException ee) {
-      String errorMessage = String
-          .format("Could not instantiate %s with a String-only constructor: %s", e.getType(),
-              ee.getMessage());
-      throw new IllegalStateException(errorMessage, ee);
-    }
-  }
-
-  /**
-   * Holds the different types of exceptions thrown by Alluxio.
-   *
-   * @deprecated since version 1.1 and will be removed in version 2.0
-   */
-  @ThreadSafe
-  @Deprecated
-  private enum AlluxioExceptionType {
-    ACCESS_CONTROL(AccessControlException.class),
-    BLOCK_ALREADY_EXISTS(BlockAlreadyExistsException.class),
-    BLOCK_DOES_NOT_EXIST(BlockDoesNotExistException.class),
-    BLOCK_INFO(BlockInfoException.class),
-    CONNECTION_FAILED(ConnectionFailedException.class),
-    DEPENDENCY_DOES_NOT_EXIST(DependencyDoesNotExistException.class),
-    DIRECTORY_NOT_EMPTY_EXCEPTION(DirectoryNotEmptyException.class),
-    FAILED_TO_CHECKPOINT(FailedToCheckpointException.class),
-    FILE_ALREADY_COMPLETED(FileAlreadyCompletedException.class),
-    FILE_ALREADY_EXISTS(FileAlreadyExistsException.class),
-    FILE_DOES_NOT_EXIST(FileDoesNotExistException.class),
-    INVALID_FILE_SIZE(InvalidFileSizeException.class),
-    INVALID_PATH(InvalidPathException.class),
-    INVALID_WORKER_STATE(InvalidWorkerStateException.class),
-    LINEAGE_DELETION(LineageDeletionException.class),
-    LINEAGE_DOES_NOT_EXIST(LineageDoesNotExistException.class),
-    NO_WORKER(NoWorkerException.class),
-    WORKER_OUT_OF_SPACE(WorkerOutOfSpaceException.class);
-
-    private final Class<? extends AlluxioException> mExceptionClass;
-
-    /**
-     * Constructs a {@link AlluxioExceptionType} with its corresponding {@link AlluxioException}.
-     */
-    AlluxioExceptionType(Class<? extends AlluxioException> exceptionClass) {
-      mExceptionClass = exceptionClass;
-    }
-
-    /**
-     * Produces an Alluxio exception whose type matches the given name.
-     *
-     * @param text the type name
-     * @return the Alluxio exception
-     */
-    static Class<? extends AlluxioException> getAlluxioExceptionClass(String text) {
-      if (text != null) {
-        for (AlluxioExceptionType t : AlluxioExceptionType.values()) {
-          if (text.equalsIgnoreCase(t.name())) {
-            return t.mExceptionClass;
-          }
-        }
-      }
-      return null;
-    }
-
-    /**
-     * Produces the type name for the type that matches the given Alluxio exception.
-     *
-     * @param e the Alluxio exception
-     * @return the type name
-     */
-    static String getAlluxioExceptionType(Class<? extends AlluxioException> e) {
-      if (e != null) {
-        for (AlluxioExceptionType t : AlluxioExceptionType.values()) {
-          if (t.mExceptionClass.equals(e)) {
-            return t.name();
-          }
-        }
-      }
-      return null;
-    }
   }
 }
--- core/common/src/main/java/alluxio/exception/status/AlluxioStatusException.java
+++ core/common/src/main/java/alluxio/exception/status/AlluxioStatusException.java
@@ -11,11 +11,14 @@
 
 package alluxio.exception.status;
 
+import alluxio.exception.AlluxioException;
+import alluxio.thrift.AlluxioTException;
+
 /**
  * An exception thrown by Alluxio. {@link #getStatus()} can be used to determine the represented
  * class of error.
  */
-public class AlluxioStatusException extends RuntimeException {
+public class AlluxioStatusException extends AlluxioException {
   private static final long serialVersionUID = -7422144873058169662L;
 
   private final ExceptionStatus mStatus;
@@ -52,5 +55,49 @@ public class AlluxioStatusException extends RuntimeException {
    */
   public ExceptionStatus getStatus() {
     return mStatus;
+  }
+
+  /**
+   * Converts an Alluxio exception from Thrift representation to native representation.
+   *
+   * @param e the Alluxio Thrift exception
+   * @return the native Alluxio exception
+   */
+  public static AlluxioStatusException fromThrift(AlluxioTException e) {
+    String m = e.getMessage();
+    switch (e.getStatus()) {
+      case Canceled:
+        return new CanceledException(m);
+      case InvalidArgument:
+        return new InvalidArgumentException(m);
+      case DeadlineExceeded:
+        return new DeadlineExceededException(m);
+      case NotFound:
+        return new NotFoundException(m);
+      case AlreadyExists:
+        return new AlreadyExistsException(m);
+      case PermissionDenied:
+        return new PermissionDeniedException(m);
+      case Unauthenticated:
+        return new UnauthenticatedException(m);
+      case ResourceExhausted:
+        return new ResourceExhaustedException(m);
+      case FailedPrecondition:
+        return new FailedPreconditionException(m);
+      case Aborted:
+        return new AbortedException(m);
+      case OutOfRange:
+        return new OutOfRangeException(m);
+      case Unimplemented:
+        return new UnimplementedException(m);
+      case Internal:
+        return new InternalException(m);
+      case Unavailable:
+        return new UnavailableException(m);
+      case DataLoss:
+        return new DataLossException(m);
+      default:
+        return new UnknownException(m);
+    }
   }
 }
