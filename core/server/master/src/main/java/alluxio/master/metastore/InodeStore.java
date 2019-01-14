44d5581164d9c0c32d4e93101d84ec1013b30fe3
Andrew Audibert <andrew@alluxio.com>
2019-01-14 14:47:24 -0800

    Add inodestore benchmark main method

--- core/server/master/src/main/java/alluxio/master/metastore/InodeStore.java
+++ core/server/master/src/main/java/alluxio/master/metastore/InodeStore.java
@@ -11,11 +11,21 @@
 
 package alluxio.master.metastore;
 
+import alluxio.PropertyKey;
+import alluxio.conf.InstancedConfiguration;
 import alluxio.master.file.meta.Inode;
 import alluxio.master.file.meta.InodeView;
 import alluxio.master.file.meta.MutableInode;
+import alluxio.master.file.meta.MutableInodeDirectory;
+import alluxio.master.file.options.CreateDirectoryOptions;
+import alluxio.master.metastore.java.HeapInodeStore;
+import alluxio.master.metastore.rocks.RocksInodeStore;
 
+import org.rocksdb.RocksDBException;
+
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Inode metadata storage.
@@ -70,4 +80,41 @@ public interface InodeStore extends ReadOnlyInodeStore {
    * @param name the child name
    */
   void removeChild(long parentId, String name);
+
+  static void main(String[] args) throws RocksDBException {
+    InstancedConfiguration diskConf = InstancedConfiguration.newBuilder()
+        .build();
+    InstancedConfiguration ramdiskConf = InstancedConfiguration.newBuilder()
+        .setProperty(PropertyKey.MASTER_METASTORE_DIR, "/Volumes/ramdisk")
+        .setProperty(PropertyKey.MASTER_METASTORE_ROCKS_IN_MEMORY, true)
+        .build();
+    for (InodeStore store : Arrays.asList(
+        new HeapInodeStore(),
+        new RocksInodeStore(diskConf),
+        new RocksInodeStore(ramdiskConf)
+    )) {
+      int numInodes = 1_000_000;
+      long s = System.currentTimeMillis();
+      writeInodes(store, 0, numInodes);
+      System.out.printf("Wrote %s inodes in %sms%n", numInodes, System.currentTimeMillis() - s);
+      // Random read benchmark
+      for (int iter = 0; iter < 5; iter++) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1_000_000; i++) {
+          long id = ThreadLocalRandom.current().nextLong(numInodes);
+          store.get(id);
+        }
+        System.out.printf("Completed 1 million random reads in %sms%n",
+            System.currentTimeMillis() - start);
+      }
+    }
+  }
+
+  static void writeInodes(InodeStore store, int startId, int count) {
+    for (int i = startId; i < startId + count; i++) {
+      MutableInodeDirectory dir =
+          MutableInodeDirectory.create(i, 0, "x", CreateDirectoryOptions.defaults());
+      store.writeInode(dir);
+    }
+  }
 }
